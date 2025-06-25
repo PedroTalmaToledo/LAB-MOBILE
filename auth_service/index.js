092e0d23f49df392e7496ba60602026ad6aa569f
Pedro Talma Toledo <pedrotoledo1717@gmail.com>
2025-06-25 16:41:26 -0300

    Feat - subindo microserviços Auth_Service para google cloud

--- auth_service/index.js
+++ auth_service/index.js
@@ -1,25 +1,28 @@
-require('dotenv').config(); 
+require("dotenv").config();
 
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const morgan = require('morgan');
-const authRoutes = require('./routes/authRoutes');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const morgan = require("morgan");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
 app.use(cors());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
 
 // Rotas de autenticação
-app.use('/auth', authRoutes);
+app.use("/auth", authRoutes);
 
 // Conexão com o MongoDB e inicialização do servidor
-const PORT = process.env.PORT || 5001;
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => app.listen(PORT, () => console.log(`Auth Service running on port ${PORT}`)))
-.catch(err => console.error('DB connection error:', err));
+const PORT = process.env.PORT || 8080;
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Auth Service running on port ${PORT}`))
+  )
+  .catch((err) => console.error("DB connection error:", err));
--- crud_pedidos/pedidos/src/main/java/br/pucminas/lab/microservicos/pedidos/config/SecurityConfig.java
+++ crud_pedidos/pedidos/src/main/java/br/pucminas/lab/microservicos/pedidos/config/SecurityConfig.java
@@ -2,7 +2,9 @@ package br.pucminas.lab.microservicos.pedidos.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class SecurityConfig {
