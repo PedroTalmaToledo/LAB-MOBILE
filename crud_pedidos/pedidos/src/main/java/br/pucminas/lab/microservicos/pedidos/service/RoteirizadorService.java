f4ebeea2a6f1bc54c9ab012560cd8b847a7cab8f
DeveloperAnaFlavia <162126385+DeveloperAnaFlavia@users.noreply.github.com>
2025-06-25 01:08:06 -0300

    Update RoteirizadorService.java

--- crud_pedidos/pedidos/src/main/java/br/pucminas/lab/microservicos/pedidos/service/RoteirizadorService.java
+++ crud_pedidos/pedidos/src/main/java/br/pucminas/lab/microservicos/pedidos/service/RoteirizadorService.java
@@ -58,27 +58,36 @@ public class RoteirizadorService {
             throw new RuntimeException("Erro ao consultar Nominatim: " + endereco, e);
         }
     }
-
     /**
      * Calcula rota entre duas coordenadas usando OSRM.
      */
-    public RotaResponse calcularRota(double origemLat, double origemLon, double destinoLat, double destinoLon) {
+public RotaResponse calcularRota(double origemLat, double origemLon, double destinoLat, double destinoLon) {
         String coords = String.format(Locale.US, "%f,%f;%f,%f", origemLon, origemLat, destinoLon, destinoLat);
-        String url = String.format("%s/%s?overview=false", OSRM_BASE_URL, coords);
+        String url = String.format("%s/%s?overview=full&geometries=geojson", OSRM_BASE_URL, coords);
         System.out.println("Requisição OSRM: " + url);
+
         try {
             String resp = restTemplate.getForObject(url, String.class);
             JsonNode root = mapper.readTree(resp);
             JsonNode route = root.path("routes").get(0);
+
             double distanciaKm = Math.round((route.path("distance").asDouble() / 1000.0) * 10.0) / 10.0;
             double segundos = route.path("duration").asDouble();
             double horas = segundos / 3600.0;
             int horasInt = (int) horas;
             int minutos = (int) Math.round((horas - horasInt) * 60);
-
             String duracaoFormatada = horasInt + "h " + minutos + "min";
 
-            return new RotaResponse(distanciaKm, duracaoFormatada);
+            List<Coordenada> rota = new java.util.ArrayList<>();
+            JsonNode coordsArray = route.path("geometry").path("coordinates");
+            for (JsonNode coord : coordsArray) {
+                double lon = coord.get(0).asDouble();
+                double lat = coord.get(1).asDouble();
+                rota.add(new Coordenada(lat, lon));
+            }
+
+            return new RotaResponse(distanciaKm, duracaoFormatada, rota);
+
         } catch (Exception e) {
             throw new RuntimeException("Erro ao consultar rota OSRM", e);
         }
